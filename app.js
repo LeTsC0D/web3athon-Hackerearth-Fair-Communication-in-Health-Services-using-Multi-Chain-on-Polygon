866d46a3122fd35272938c8c77d6e9414041326a
shashank <shashank.srivastava5523@gmail.com>
2022-11-05 03:59:23 +0530

    10th commit

--- app.js
+++ app.js
@@ -14,5 +14,9 @@ app.use(bodyParser.urlencoded({
 
 app.use('/', indexRouter);
 
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Listening on Port, ${port}`);
+});
 
-const server=app.listen(3000,()=>console.log('server started '));
+// const server=app.listen(3000,()=>console.log('server started '));
